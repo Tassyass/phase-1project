ff49b2f604a95409ad7ffeadddbfea8890978a44
Abaadir <abaadir.yasin@student.moringaschool.com>
2023-10-24 20:58:15 +0300

    completed

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function registerUser() {
     const name = document.getElementById("name").value;
     const age = parseInt(document.getElementById("age").value);
     const interests = document.getElementById("interests").value.split(",").map(item => item.trim());
+
+    if (name && age && interests.length > 0) {
+        const user = { id: userIdCounter++, name, age, interests };
+        users.push(user);
+        alert("Registration successful!");
+    } else {
+        alert("Please fill in all fields.");
+    }
+}
